874b5befd6f212fa4ae9d01fd2f9f748d35ee9bd
vadimshut <vadimshut1992@gmail.com>
2021-03-13 02:08:47 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const holder = [];
+    const open = [];
+    const close = [];
+    bracketsConfig.forEach((e) => {
+      open.push(e[0]);
+      close.push(e[1]);
+    });
+  
+    for (let i of str) {      
+      if (holder.includes(i) && close.includes(i)) {       
+        holder.splice(-1, 1);
+      } else if (open.includes(i)) {        
+        holder.push(i);
+      } else if (close.includes(i)) {       
+        const openPair = open[close.indexOf(i)]; 
+        if (holder[holder.length - 1] === openPair) {
+         
+          holder.splice(-1, 1); 
+        } else {          
+          holder.push(i);
+          break; 
+        }
+      }
+    }
+    return holder.length === 0; 
+  };
